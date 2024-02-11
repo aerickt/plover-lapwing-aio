9721b7a469490f774b8e7fbec668910b16e868c4
aerick <aericktt@gmail.com>
2024-02-11 07:10:54 -0800

    add separate commands and nouns dictionary

--- plover_lapwing/system.py
+++ plover_lapwing/system.py
@@ -116,4 +116,4 @@ KEYMAPS = {
 }
 
 DICTIONARIES_ROOT = 'asset:plover_lapwing:dictionaries'
-DEFAULT_DICTIONARIES = ('user.json', 'movement.modal', 'jeff-phrasing.py', 'abby-left-hand-modifiers.py', 'emily-modifiers.py', 'emily-symbols.py', 'lapwing-numbers.json', 'lapwing-uk-additions.json', 'lapwing-prefixed-proper-nouns.json', 'lapwing-base.json')
+DEFAULT_DICTIONARIES = ('user.json', 'jeff-phrasing.py', 'abby-left-hand-modifiers.py', 'emily-modifiers.py', 'emily-symbols.py', 'lapwing-movement.modal', 'lapwing-commands.json', 'lapwing-numbers.json', 'lapwing-uk-additions.json', 'lapwing-proper-nouns.json', 'lapwing-base.json')
