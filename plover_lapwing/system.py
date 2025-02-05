13dced33e07428c624d89e1c696a357add56ee63
164182 <camden.possinger@delta.com>
2025-02-05 15:41:09 -0500

    add stenotype variables

--- plover_lapwing/system.py
+++ plover_lapwing/system.py
@@ -1,5 +1,36 @@
 from plover.system.english_stenotype import *
 
+KEYS = (
+    "#",
+    "^-",
+    "+-",
+    "S-",
+    "T-",
+    "K-",
+    "P-",
+    "W-",
+    "H-",
+    "R-",
+    "A-",
+    "O-",
+    "@",
+    "*",
+    "-E",
+    "-U",
+    "-F",
+    "-R",
+    "-P",
+    "-B",
+    "-L",
+    "-G",
+    "-T",
+    "-S",
+    "-D",
+    "-Z",
+)
+
+IMPLICIT_HYPHEN_KEYS = ("A-", "O-", "-E", "-U", "@", "*")
+
 NUMBER_KEY = None
 
 NUMBERS = {}
